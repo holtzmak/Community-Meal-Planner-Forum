73478d1a78277328b464a50cc28906e02a5a7cae
Kelly Holtzman <holtzmak@uregina.ca>
2021-03-02 20:56:27 -0600

    Add silly ad-hoc smoke test to main to prove JSON works and debug things

--- app/lib/core/post.dart
+++ app/lib/core/post.dart
@@ -1,45 +1,38 @@
 import 'package:flutter/cupertino.dart';
 
-import 'account.dart';
-
 @immutable
 class Post {
-  final int postId;
-  final Account author;
+  final String authorUsername;
   final String message;
   final DateTime postDate;
 
   Post(
-      {required this.postId,
-      required this.author,
+      {required this.authorUsername,
       required this.message,
       required this.postDate});
 
   Post.fromJson(Map<String, dynamic> json)
-      : postId = json['postId'],
-        author = Account.fromJson(json['author']),
+      : authorUsername = json['authorUsername'],
         message = json['message'],
         postDate = json['postDate'].toDate();
 
   Map<String, dynamic> toJson() => {
-        'postId': postId,
-        'author': author.toJson(),
+        'authorUsername': authorUsername,
         'message': message,
         'postDate': postDate
       };
 
   @override
   int get hashCode =>
-      postId.hashCode ^ author.hashCode ^ message.hashCode ^ postDate.hashCode;
+      authorUsername.hashCode ^ message.hashCode ^ postDate.hashCode;
 
   @override
   bool operator ==(other) {
     return (other is Post) &&
-        other.postId == postId &&
-        other.author == author &&
+        other.authorUsername == authorUsername &&
         other.message == message &&
         other.postDate == postDate;
   }
 
-  String toString() => 'Post($postId, $author, $message, $postDate)';
+  String toString() => 'Post($authorUsername, $message, $postDate)';
 }
