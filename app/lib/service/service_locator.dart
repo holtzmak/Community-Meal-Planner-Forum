89519929877f0287667db61e7d494fc1efb2ad94
Kelly Holtzman <holtzmak@uregina.ca>
2021-03-07 21:20:30 -0600

    Add Firebase-related Services

--- app/lib/service/service_locator.dart
+++ app/lib/service/service_locator.dart
@@ -1,7 +1,18 @@
+import 'package:app/service/firebase_auth_service.dart';
+import 'package:app/service/firebase_database_service.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:get_it/get_it.dart';
 
 class ServiceLocator {
   static final it = GetIt.instance;
 
-  static void setup() {}
+  static void setup() {
+    it.registerLazySingleton<FirebaseAuth>(() => FirebaseAuth.instance);
+    it.registerLazySingleton<FirebaseFirestore>(
+        () => FirebaseFirestore.instance);
+    it.registerLazySingleton<FirebaseDatabaseService>(
+        () => FirebaseDatabaseService());
+    it.registerLazySingleton<FirebaseAuthService>(() => FirebaseAuthService());
+  }
 }
