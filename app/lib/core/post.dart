1a7c3ca5a592c645385ffff50746b912789edc09
Kelly Holtzman <holtzmak@uregina.ca>
2021-03-07 20:04:03 -0600

    Updates to core types to match database structure

--- app/lib/core/post.dart
+++ app/lib/core/post.dart
@@ -2,37 +2,49 @@ import 'package:flutter/cupertino.dart';
 
 @immutable
 class Post {
-  final String authorUsername;
+  final String id; // Firebase Firestore ID
+  final String authorId;
   final String message;
   final DateTime postDate;
 
   Post(
-      {required this.authorUsername,
+      {required this.id,
+      required this.authorId,
       required this.message,
       required this.postDate});
 
-  Post.fromJson(Map<String, dynamic> json)
-      : authorUsername = json['authorUsername'],
+  Post.empty(String id)
+      : id = id,
+        authorId = "",
+        message = "",
+        postDate = DateTime.now();
+
+  // Optional ID for completion posts, which cannot be modified
+  Post.fromJson({String? id, required Map<String, dynamic> json})
+      : id = id ?? "",
+        authorId = json['authorId'],
         message = json['message'],
         postDate = json['postDate'].toDate();
 
-  Map<String, dynamic> toJson() => {
-        'authorUsername': authorUsername,
-        'message': message,
-        'postDate': postDate
-      };
+  Map<String, dynamic> toJson() =>
+      {'authorId': authorId, 'message': message, 'postDate': postDate};
 
   @override
   int get hashCode =>
-      authorUsername.hashCode ^ message.hashCode ^ postDate.hashCode;
+      id.hashCode ^ authorId.hashCode ^ message.hashCode ^ postDate.hashCode;
 
   @override
   bool operator ==(other) {
     return (other is Post) &&
-        other.authorUsername == authorUsername &&
+        other.id == id &&
+        other.authorId == authorId &&
         other.message == message &&
         other.postDate == postDate;
   }
 
-  String toString() => 'Post($authorUsername, $message, $postDate)';
+  String toString() =>
+      'Post(id: $id, authorId: $authorId, message: $message, postDate: $postDate)';
+
+  Post withDocumentId(String newId) =>
+      Post(id: newId, authorId: authorId, message: message, postDate: postDate);
 }
--- app/lib/core/thread.dart
+++ app/lib/core/thread.dart
@@ -7,29 +7,41 @@ import 'package:flutter/foundation.dart';
 
 @immutable
 class Thread {
+  final String id; // Firebase Firestore ID
   final String title;
   final List<Topic> topics;
   final List<SubTopic> subTopics;
-  final String authorUsername;
+  final String authorId;
   final DateTime startDate;
   final DateTime? completionDate;
   final Post? completionPost;
-  final List<Post> posts;
   final bool canBeRepliedTo;
 
   Thread(
-      {required this.title,
+      {required this.id,
+      required this.title,
       required this.topics,
       required this.subTopics,
-      required this.authorUsername,
+      required this.authorId,
       required this.startDate,
       required this.completionDate,
       required this.completionPost,
-      required this.posts,
       required this.canBeRepliedTo});
 
-  Thread.fromJson(Map<String, dynamic> json)
-      : title = json['title'],
+  Thread.empty(String id)
+      : id = id,
+        title = "",
+        topics = [],
+        subTopics = [],
+        authorId = "",
+        startDate = DateTime.now(),
+        completionDate = null,
+        completionPost = null,
+        canBeRepliedTo = false;
+
+  Thread.fromJson({required String id, required Map<String, dynamic> json})
+      : id = id,
+        title = json['title'],
         topics = json['topics']
             .map<Topic>((topic) => TopicDeserializer.fromString(topic))
             .toList(),
@@ -37,69 +49,84 @@ class Thread {
             .map<SubTopic>(
                 (subTopic) => SubTopicDeserializer.fromString(subTopic))
             .toList(),
-        authorUsername = json['authorUsername'],
+        authorId = json['authorId'],
         startDate = json['startDate'].toDate(),
         completionDate = NullableJsonConverter().getFromJsonMaybe(
             json: json['completionDate'], transform: (it) => it.toDate()),
         completionPost = NullableJsonConverter().getFromJsonMaybe(
-            json: json['completionPost'], transform: (it) => Post.fromJson(it)),
-        posts = json['posts'].map<Post>((post) => Post.fromJson(post)).toList(),
+            json: json['completionPost'],
+            transform: (it) => Post.fromJson(json: it)),
         canBeRepliedTo = json['canBeRepliedTo'];
 
   Map<String, dynamic> toJson() => {
         'title': title,
         'topics': topics.map((it) => it.toString()).toList(),
         'subTopics': subTopics.map((it) => it.toString()).toList(),
-        'authorUsername': authorUsername,
+        'authorId': authorId,
         'startDate': startDate,
         'completionDate': completionDate,
         'completionPost': completionPost?.toJson(),
-        'posts': posts.map((post) => post.toJson()).toList(),
         'canBeRepliedTo': canBeRepliedTo
       };
 
   @override
   int get hashCode =>
+      id.hashCode ^
       title.hashCode ^
       topics.hashCode ^
       subTopics.hashCode ^
-      authorUsername.hashCode ^
+      authorId.hashCode ^
       startDate.hashCode ^
       completionDate.hashCode ^
       completionPost.hashCode ^
-      posts.hashCode ^
       canBeRepliedTo.hashCode;
 
   @override
   bool operator ==(other) {
     return (other is Thread) &&
+        other.id == id &&
         other.title == title &&
         listEquals(other.topics, topics) &&
         listEquals(other.subTopics, subTopics) &&
-        other.authorUsername == authorUsername &&
+        other.authorId == authorId &&
         other.startDate == startDate &&
         other.completionDate == completionDate &&
         other.completionPost == completionPost &&
-        listEquals(other.posts, posts) &&
         other.canBeRepliedTo == canBeRepliedTo;
   }
 
   String toString() =>
-      'Thread($title, $topics, $subTopics, $authorUsername, $startDate, $completionDate, $completionPost, $posts, $canBeRepliedTo)';
+      'Thread(id: $id, title: $title, topics: $topics, subTopics: $subTopics, authorId: $authorId, startDate: $startDate, completionDate: $completionDate, completionPost: $completionPost, canBeRepliedTo: $canBeRepliedTo)';
+
+  Thread withDocumentId(String newId) {
+    if (completionDate == null) {
+      throw Exception("Cannot add topics to a completed Thread");
+    }
+    return Thread(
+        id: newId,
+        title: title,
+        topics: topics,
+        subTopics: subTopics,
+        authorId: authorId,
+        startDate: startDate,
+        completionDate: completionDate,
+        completionPost: completionPost,
+        canBeRepliedTo: canBeRepliedTo);
+  }
 
   Thread withTopics(List<Topic> newTopics) {
     if (completionDate == null) {
       throw Exception("Cannot add topics to a completed Thread");
     }
     return Thread(
+        id: id,
         title: title,
         topics: newTopics,
         subTopics: subTopics,
-        authorUsername: authorUsername,
+        authorId: authorId,
         startDate: startDate,
         completionDate: completionDate,
         completionPost: completionPost,
-        posts: posts,
         canBeRepliedTo: canBeRepliedTo);
   }
 
@@ -108,14 +135,14 @@ class Thread {
       throw Exception("Cannot add sub topics to a completed Thread");
     }
     return Thread(
+        id: id,
         title: title,
         topics: topics,
         subTopics: newSubTopics,
-        authorUsername: authorUsername,
+        authorId: authorId,
         startDate: startDate,
         completionDate: completionDate,
         completionPost: completionPost,
-        posts: posts,
         canBeRepliedTo: canBeRepliedTo);
   }
 
@@ -123,40 +150,24 @@ class Thread {
           {required DateTime newCompletionDate,
           required Post? newCompletionPost}) =>
       Thread(
+          id: id,
           title: title,
           topics: topics,
           subTopics: subTopics,
-          authorUsername: authorUsername,
+          authorId: authorId,
           startDate: startDate,
           completionDate: newCompletionDate,
           completionPost: newCompletionPost,
-          posts: posts,
           canBeRepliedTo: false);
 
   Thread restoredAsIncomplete({required bool canBeRepliedTo}) => Thread(
+      id: id,
       title: title,
       topics: topics,
       subTopics: subTopics,
-      authorUsername: authorUsername,
+      authorId: authorId,
       startDate: startDate,
       completionDate: null,
       completionPost: null,
-      posts: posts,
       canBeRepliedTo: canBeRepliedTo);
-
-  Thread withPosts(List<Post> newPosts) {
-    if (completionDate == null) {
-      throw Exception("Cannot add posts to a completed Thread");
-    }
-    return Thread(
-        title: title,
-        topics: topics,
-        subTopics: subTopics,
-        authorUsername: authorUsername,
-        startDate: startDate,
-        completionDate: completionDate,
-        completionPost: completionPost,
-        posts: newPosts,
-        canBeRepliedTo: canBeRepliedTo);
-  }
 }
