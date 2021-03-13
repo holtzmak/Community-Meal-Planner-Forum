7b04107b4ce78c82821c0800ce6af0c9ac2b7fe7
Kelly Holtzman <holtzmak@uregina.ca>
2021-03-13 00:24:47 -0600

    Update DynamicFormField to use validation

--- app/lib/ui/widget/form/string_dynamic_form_field.dart
+++ app/lib/ui/widget/form/string_dynamic_form_field.dart
@@ -8,11 +8,11 @@ import 'dynamic_form_field.dart';
 /// A custom form field for List<String>.
 /// Due to it's dynamic nature this widget should only be used inside a grow-able
 /// widget, like column, and not inside static widgets like ListTiles.
-DynamicFormField<String, StringFormField> dynamicStringFormField(
+DynamicFormField<String> dynamicStringFormField(
         {required List<String> initialList,
         required String titles,
         required Function(List<String>) onSaved}) =>
-    DynamicFormField<String, StringFormField>(
+    DynamicFormField<String>(
         initialList: initialList,
         titles: titles,
         onSaved: onSaved,
@@ -22,7 +22,7 @@ DynamicFormField<String, StringFormField> dynamicStringFormField(
             StringFormField(
               // Must have unique keys in rebuilding widget lists
               key: ObjectKey(Uuid().v4()),
-              initial: it,
+              initialValue: it,
               title: titles,
               onSaved: (String? changed) => onSaved(index, changed),
               onDelete: () => onDelete(index),
--- app/lib/ui/widget/form/string_form_field.dart
+++ app/lib/ui/widget/form/string_form_field.dart
@@ -2,71 +2,78 @@ import 'package:app/ui/style.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-/// A custom FormField<String> with onSaved and onDelete callback
-class StringFormField extends StatefulWidget {
-  final String initial;
-  final String title;
-  final bool isMultiline;
-  final FormFieldSetter<String> onSaved;
-  final Function()? onDelete;
-  final FormFieldValidator<String> validator;
-
-  // TODO: Rework this class so as to be able to call validate, save on it
+class StringFormField extends FormField<String> {
   StringFormField(
       {Key? key,
-      required this.initial,
-      required this.title,
-      required this.onSaved,
-      required this.validator,
-      this.onDelete,
-      this.isMultiline = false})
-      : super(key: key);
-
-  @override
-  _StringFormFieldState createState() => _StringFormFieldState();
+      required String initialValue,
+      required String title,
+      required FormFieldSetter<String> onSaved,
+      required Function()? onDelete,
+      required FormFieldValidator<String> validator,
+      bool isMultiline = false})
+      : super(
+            key: key,
+            onSaved: onSaved,
+            validator: validator,
+            initialValue: initialValue,
+            builder: (FormFieldState<String> state) {
+              return CustomFormFieldState(
+                state: state,
+                title: title,
+                isMultiline: isMultiline,
+                onSaved: onSaved,
+                onDelete: onDelete,
+              );
+            });
 }
 
-class _StringFormFieldState extends State<StringFormField> {
+class CustomFormFieldState extends StatelessWidget {
+  final FormFieldState<String> state;
+  final String title;
+  final bool isMultiline;
+  final Function()? onDelete;
+  final FormFieldSetter<String> onSaved;
+
+  CustomFormFieldState({
+    required this.state,
+    required this.title,
+    required this.isMultiline,
+    required this.onDelete,
+    required this.onSaved,
+  });
+
   @override
   Widget build(BuildContext context) {
-    return widget.onDelete != null
-        ? ListTile(
-            title: TextFormField(
-              validator: widget.validator,
-              decoration: InputDecoration(
-                  errorStyle: GoogleFonts.roboto(color: BurntSienna),
-                  border: OutlineInputBorder(),
-                  labelText: widget.title),
-              keyboardType: widget.isMultiline
-                  ? TextInputType.multiline
-                  : TextInputType.text,
-              // Needed for the text field to expand
-              maxLines: null,
-              initialValue: widget.initial,
-              onChanged: widget.onSaved,
-              onSaved: widget.onSaved,
+    return Stack(
+      children: [
+        ListTile(
+          title: TextFormField(
+            initialValue: state.value,
+            decoration:
+                InputDecoration(border: OutlineInputBorder(), labelText: title),
+            keyboardType:
+                isMultiline ? TextInputType.multiline : TextInputType.text,
+            // Needed for the text field to expand
+            maxLines: null,
+            onSaved: onSaved,
+          ),
+          trailing: onDelete != null
+              ? IconButton(
+                  icon: Icon(Icons.delete_forever, size: 40.0),
+                  onPressed: onDelete,
+                )
+              : Container(),
+        ),
+        if (state.hasError)
+          Padding(
+            padding: EdgeInsets.only(top: 65),
+            child: Text(
+              state.errorText!,
+              textAlign: TextAlign.left,
+              style: GoogleFonts.notoSerif(color: BurntSienna),
             ),
-            trailing: IconButton(
-              icon: Icon(Icons.delete_forever, size: 40.0),
-              onPressed: widget.onDelete,
-            ),
-          )
-        : ListTile(
-            title: TextFormField(
-              validator: widget.validator,
-              decoration: InputDecoration(
-                  errorStyle: GoogleFonts.roboto(color: BurntSienna),
-                  border: OutlineInputBorder(),
-                  labelText: widget.title),
-              keyboardType: widget.isMultiline
-                  ? TextInputType.multiline
-                  : TextInputType.text,
-              // Needed for the text field to expand
-              maxLines: null,
-              initialValue: widget.initial,
-              onSaved: widget.onSaved,
-              onChanged: widget.onSaved,
-            ),
-          );
+          ),
+      ],
+    );
   }
 }
